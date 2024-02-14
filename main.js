6913932838c0971c1562aee45df50df0337fcf74
Herstam <jettarin.pattana@gmail.com>
2024-02-14 21:54:03 +0700

    welcome to my git

---
+++ main.js
@@ -0,0 +1,22 @@
+function calculation_grade(score){
+    if (score >= 80){
+        grade = 'A'
+    }
+    else if(score >= 70){
+        grade = 'B'
+    }
+    else if(score >= 60){
+        grade = 'CB'
+    }
+    else if(score >= 50){
+        grade = 'D'
+    }
+    else{
+        grade = 'F'
+    }
+    return grade
+}
+
+let student1 = calculation_grade(75)
+
+console.log(student1)
